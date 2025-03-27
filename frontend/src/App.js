a493b1b94a77f820453385a8d5427068b85d9d7a
AlanSalazar20003 <149444584+AlanSalazar20003@users.noreply.github.com>
2025-03-27 09:43:24 -0600

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UploadExcel from "./UploadExcel";
+
+function App() {
+  return (
+    <div>
+      <h1>Sube tu archivo Excel</h1>
+      <UploadExcel />
+    </div>
+  );
+}
+
+export default App;
